50918e2999aa45e97f097b99cdc72ee59a9c59ad
jacobdavis1 <jacob.marquez.davis@hotmail.com>
2020-08-18 15:09:16 -0400

    Added comments

--- Exceptions/src/MyPackage/App.java
+++ Exceptions/src/MyPackage/App.java
@@ -9,15 +9,25 @@ public class App {
 		// so no try/catch needed
 		Object o = new Object();
 		o = null;
-		// Problem.uncheckedIterate(o);
+		// Throws a NullPointerExeption
+		Problem.uncheckedIterate(o);
 		
+		// Try is not optional if you need to handle
+		// a checked exception
 		try {
+			// Throws an Exception
 			Problem.iterate();
+			
+			// Throws an ArithmeticException
 			// int f = Problem.divideBad(5, 0);
 			
 		} catch (ArithmeticException ae) {
 			System.out.println("Cant divide by 0 yo: " + ae.getMessage());
-		} catch (Exception e) {
+		} 
+		// You should probably have a general Exception catch block
+		// but as long as you handle all specific checked exception
+		// possibilities, you don't need it
+		catch (Exception e) {
 			System.out.println(e.getMessage());
 			e.printStackTrace();
 		} 
--- Exceptions/src/MyPackage/Problem.java
+++ Exceptions/src/MyPackage/Problem.java
@@ -2,26 +2,34 @@ package MyPackage;
 
 public class Problem {
 	
+	// This method throws an Exception when run, which
+	// means any method that uses it must acknowledge its
+	// checked exception payload!
 	public static void throwAnException(String message) throws Exception {
 		throw new Exception(message);
 	}
 	
+	// Attempts to throw 10 exceptions
 	public static void iterate() throws Exception {
 		for (int i = 0; i < 10; ++i) {
 			throwAnException("Exception: The number is " + i);
 		}
 	}
 	
+	// Tries to print Object o 10 times. If o is null, exception!
 	public static void uncheckedIterate(Object o) {
 		for (int i = 0; i < 10; ++i) {
 			System.out.println(o.toString());
 		}
 	}
 	
+	// If m is 0, exception!
 	public static int divideBad(int n, int m) {
 		return n / m;
 	}
 	
+	// This version catches the ArithmeticException
+	// and returns 0
 	public static int divideGood(int n, int m) {
 		try {
 			return n / m;
